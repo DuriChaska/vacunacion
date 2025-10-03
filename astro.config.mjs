64766c58575bfe35c528bc289fb383cf2744bd6b
DuriChaska <kimduri2004reina@gmail.com>
2025-10-03 10:19:26 -0600

    Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://DuriChaska.github.io',
+  base: '/vacunacion/',
 });
